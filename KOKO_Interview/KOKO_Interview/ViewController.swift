a5a26a38cb1f294929bfca74b445abbae7c4bf8f
Wei-Jen, Wang <weijen.wang@tpisoftware.com>
2023-10-25 10:16:04 +0800

    initial commit

---
+++ KOKO_Interview/KOKO_Interview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KOKO_Interview
+//
+//  Created by TPI on 2023/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
