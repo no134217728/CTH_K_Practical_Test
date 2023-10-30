692a8da238cffbb689f0a07869c2bded886b31b9
Wei-Jen, Wang <weijen.wang@tpisoftware.com>
2023-10-30 11:29:37 +0800

    feat: add the tab bar default style

---
+++ KOKO_Interview/KOKO_Interview/Pages/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarController.swift
+//  KOKO_Interview
+//
+//  Created by TPI on 2023/10/30.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        selectedIndex = 1
+    }
+}
